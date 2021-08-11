f7f25b5ec9d1fba003e805ea0f8c2c5c14a6d77e
Sabita Tasnim <sabita.tasnim@gmail.com>
2021-08-11 03:54:46 -0400

    Added model 2, not sure if this is final code

--- A3/futoshiki_csp.py
+++ A3/futoshiki_csp.py
@@ -24,9 +24,11 @@ cell of the Futoshiki puzzle.
 '''
 from cspbase import *
 import itertools
-
-# Need to extract variables in order to use for other functions creating a list of lists in order to map coordinates to the futo grid and 
-# Dealing with the row/col constraints are much easier to iterate though as an array (without the binary inequality constraints)
+'''
+* Need to extract variables in order to use for other functions creating a list of lists in order to map coordinates to the futo grid
+* Dealing with the row/col constraints are much easier to iterate though as an array (without the binary inequality constraints)
+* Return type for the models are also arrays
+'''
 def extract_variables(futo_grid):
     # we know that the futo grid is n x n 
     n = len(futo_grid)
@@ -71,6 +73,10 @@ def extract_variables(futo_grid):
     # Returns both variable array (for easier tracking for adding row/col constraints + mapping to the futo_grid) and variable list for the return for meach model
     return variable_array, variable_list
 
+'''
+####################### Binary not equal constraints for the row and column constraints ########################
+
+'''
 def add_binary_row_and_col_constraints_to_csp(futo_grid, variables_array, csp, is_row):
     # Find size of futo grid 
     n = len(futo_grid)
@@ -107,6 +113,48 @@ def add_binary_row_and_col_constraints_to_csp(futo_grid, variables_array, csp, i
             # Then add the constraints to the csp
             csp.add_constraint(c=constraint)
 
+'''
+########################### n-ary all-different constraints for the row and column constraints ###########################
+
+'''
+def add_nary_row_col_contraints_to_csp(futo_grid, variables_array, csp, is_row):
+    # Find size of futo grid 
+    n = len(futo_grid)
+
+    for i in range(n):
+        constraint = None
+        satisfying_tuples_list = []
+        constraint_domain = []
+
+
+        # Check if we have a row constraint
+        if is_row:
+            # Get all the row domains in a list of list to pass into itertools
+            for d in variables_array[i]:
+                constraint_domain.append(d.cur_domain())
+            c_name = "all-diff row constraint at row: " + str(i)
+            scope = variables_array[i]
+
+        # Check if have a column constraint
+        else:
+            # Getting col domains require us to parse a different row but same column 
+            col_variables = list(map(lambda x : x[i], variables_array))
+            for d in col_variables:
+                constraint_domain.append(d.cur_domain())
+            c_name = "all-diff col constraint at col: " + str(i)
+            scope = col_variables
+
+        for product in itertools.product(*constraint_domain):
+            if len(product) == len(set(product)) and len(product) == n: 
+                satisfying_tuples_list.append(product)
+
+        constraint = Constraint(name=c_name, scope=scope)
+
+        # Finally add all the satifying tuples to the constraint
+        constraint.add_satisfying_tuples(tuples=satisfying_tuples_list)
+
+        # Then add the constraints to the csp
+        csp.add_constraint(c=constraint)
 
 def add_binary_inequality_constraints_to_csp(futo_grid, variables_array, csp):
     # need to find the size of the futo grid again
@@ -175,10 +223,10 @@ def futoshiki_csp_model_1(futo_grid):
     csp_model1 = CSP(name="CSP: Model 1 - Binary Constraints only", vars=variables_list)
 
     # Add the row and column constraints
-    # ROW CONSTRAINTS
+    # ROW CONSTRAINTS (is_row=True)
     add_binary_row_and_col_constraints_to_csp(futo_grid, variables_array, csp_model1, True)
 
-    # COL CONSTRAINTS
+    # COL CONSTRAINTS (is_row=False)
     add_binary_row_and_col_constraints_to_csp(futo_grid, variables_array, csp_model1, False)
 
     # Add the inequality constraints as well
@@ -188,5 +236,27 @@ def futoshiki_csp_model_1(futo_grid):
 
 def futoshiki_csp_model_2(futo_grid):
     ##IMPLEMENT 
-    return None, None
+    variables_array = []
+    variables_list = []
+    csp_model2 = None
+    # Get the variables first as an array and as a list
+    variables_array, variables_list = extract_variables(futo_grid=futo_grid)
+
+    # Create the CSP for this model
+    csp_model2 = CSP(name="CSP: Model 2 - N-ary Constraints", vars=variables_list)
+
+    # Add the row and column constraints
+    # ROW CONSTRAINTS (is_row=True)
+    add_nary_row_col_contraints_to_csp(futo_grid, variables_array, csp_model2, True)
+
+    # COL CONSTRAINTS (is_row=False)
+    add_nary_row_col_contraints_to_csp(futo_grid, variables_array, csp_model2, False)
+
+    # Add the inequality constraints as well
+    add_binary_inequality_constraints_to_csp(futo_grid, variables_array, csp_model2)
+
+
+    return csp_model2, variables_array
    
+# board_1 = [[1,'<',0,'.',0],[0,'.',0,'.',2],[2,'.',0,'>',0]]
+# print(futoshiki_csp_model_1(board_1))
