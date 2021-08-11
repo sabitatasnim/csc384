8cd1cab218c0a3bd6a20661a04537719aa89549f
Sabita Tasnim <sabita.tasnim@gmail.com>
2021-08-11 02:08:32 -0400

    Added Model 1

--- A3/futoshiki_csp.py
+++ A3/futoshiki_csp.py
@@ -25,10 +25,166 @@ cell of the Futoshiki puzzle.
 from cspbase import *
 import itertools
 
+# Need to extract variables in order to use for other functions creating a list of lists in order to map coordinates to the futo grid and 
+# Dealing with the row/col constraints are much easier to iterate though as an array (without the binary inequality constraints)
+def extract_variables(futo_grid):
+    # we know that the futo grid is n x n 
+    n = len(futo_grid)
+    # the number of coloumns in a row 
+    col = n + n - 1
+    # The indices of the variables only so we can save those indices for later in the string
+    var_indices = range(0, col, 2)
+
+    # create the domain for the variables 1 to n inclusive 
+    domain = range(1,n+1)
+    domain = list(domain)
+    
+    # board_1 = [[1,'<',0,'.',0],[0,'.',0,'.',2],[2,'.',0,'>',0]]
+
+    variable_array = []
+    variable_list = []
+    # create the list of lists with only the variables but not the lists 
+    for i in range(n):
+        #variable_row = []
+        for j in var_indices:
+            variable = None
+            value = futo_grid[i][j]
+            var_name = "var " + str(i) + " " + str(j)
+
+            # Need to check if our futo grid value is actually 0 or not because need to assign the domain based on that
+            if value == 0:
+                # Save the indices of the variable with each name so it's easier to find the location of each variable in the futo grid 
+                variable = Variable(name=var_name, domain=domain)
+            else:
+                variable = Variable(name=var_name, domain=[value])
+            variable_list.append(variable)
+
+    # Turn the list into an array 
+    variable_array = [variable_list[i : i+n] for i in range(0, len(variable_list), n)]
+        #     variable_row.append(variable)
+        # variable_array.append(variable_row)
+    
+    # Extract the variables all in one list as well instead of a list of list to ensure we can also pass it into the CSP
+    # variable_list = itertools.chain.from_iterable(variable_array)
+    # variable_list = list(variable_list)
+
+    # Returns both variable array (for easier tracking for adding row/col constraints + mapping to the futo_grid) and variable list for the return for meach model
+    return variable_array, variable_list
+
+def add_binary_row_and_col_constraints_to_csp(futo_grid, variables_array, csp, is_row):
+    # Find size of futo grid 
+    n = len(futo_grid)
+
+    for i in range(n):
+        # THIS IS NEEDED TO ENSURE THAT WE ARE TAKING INTO ACCOUNT THE ROW/COL CONSTRAINTS (EVERY POSSIBLE COMBINATION) but in pairs 
+        for j, k in itertools.combinations(range(n), 2):
+            constraint = None
+            satisfying_tuples_list = []
+            # Check if it's a row constraint
+            if is_row:
+                var_left = variables_array[i][j]
+                var_right = variables_array[i][k]
+                c_name = "row constraint: " + str(i) + " " + str(j) + " and " + str(i) + " " + str(k) 
+            else:
+                var_left = variables_array[j][i]
+                var_right = variables_array[k][i]
+                c_name = "col constraint: " + str(j) + " " + str(i) + " and " + str(k) + " " + str(i) 
+
+            # Define the scope after finding the variables on the left and right 
+            scope = [var_left, var_right]
+            # Compile all the satisfying tuples for the constraints
+            for val_left in var_left.cur_domain():
+                for val_right in var_right.cur_domain():
+                    if val_left != val_right:
+                        pair = (val_left,val_right)
+                        satisfying_tuples_list.append(pair)
+            
+            constraint = Constraint(name=c_name, scope=scope)
+
+            # Finally add all the satifying tuples to the constraint
+            constraint.add_satisfying_tuples(tuples=satisfying_tuples_list)
+
+            # Then add the constraints to the csp
+            csp.add_constraint(c=constraint)
+
+
+def add_binary_inequality_constraints_to_csp(futo_grid, variables_array, csp):
+    # need to find the size of the futo grid again
+    n = len(futo_grid)
+
+    for i in range(n):
+        for j in range(n - 1): # n - 1 to prevent unacessed variables in right variable
+            var_left = variables_array[i][j]
+            var_right = variables_array[i][j+1]
+            # This is where the mapping from the n x n variable array is being used hence needing an array for variables
+            ineq_constraint = futo_grid[i][2*j+1]
+            constraint = None
+            # Define the scope of the constraint to put into the constraint
+            scope = [var_left, var_right]
+            satisfying_tuples_list = []
+            
+            # Check if we have a greater than constraint 
+            if ineq_constraint == ">":
+                # Compile all the satisfying tuples for the constraints
+                for val_left in var_left.cur_domain():
+                    for val_right in var_right.cur_domain():
+                        if val_left > val_right:
+                            pair = (val_left,val_right)
+                            satisfying_tuples_list.append(pair)
+                
+                # Now create the constraint
+                c_name = "constraint: > " + str(i) + " " + str(j)
+                constraint = Constraint(name=c_name, scope=scope)
+
+            # Check if we have a less than constraint   
+            elif ineq_constraint == "<":
+                # Compile all the satisfying tuples for the constraints
+                for val_left in var_left.cur_domain():
+                    for val_right in var_right.cur_domain():
+                        if val_left < val_right:
+                            pair = (val_left,val_right)
+                            satisfying_tuples_list.append(pair)
+                
+                # Now create the constraint
+                c_name = "constraint: < " + str(i) + " " + str(j)
+                constraint = Constraint(name=c_name, scope=scope)
+
+            # Check if we have no constraint 
+            else:
+                continue
+
+            # Finally add all the satifying tuples to the constraint
+            constraint.add_satisfying_tuples(tuples=satisfying_tuples_list)
+
+            # Then add the constraints to the csp
+            csp.add_constraint(c=constraint)
+
+# board_1 = [[1,'<',0,'.',0],[0,'.',0,'.',2],[2,'.',0,'>',0]]
+# a, b= extract_variables(board_1)
+
+
 def futoshiki_csp_model_1(futo_grid):
     ##IMPLEMENT
-    return None, None
-    
+    variables_array = []
+    variables_list = []
+    csp_model1 = None
+    # Get the variables first as an array and as a list
+    variables_array, variables_list = extract_variables(futo_grid=futo_grid)
+
+    # Create the CSP for this model
+    csp_model1 = CSP(name="CSP: Model 1 - Binary Constraints only", vars=variables_list)
+
+    # Add the row and column constraints
+    # ROW CONSTRAINTS
+    add_binary_row_and_col_constraints_to_csp(futo_grid, variables_array, csp_model1, True)
+
+    # COL CONSTRAINTS
+    add_binary_row_and_col_constraints_to_csp(futo_grid, variables_array, csp_model1, False)
+
+    # Add the inequality constraints as well
+    add_binary_inequality_constraints_to_csp(futo_grid, variables_array, csp_model1)
+
+    return csp_model1, variables_array
 
 def futoshiki_csp_model_2(futo_grid):
     ##IMPLEMENT 
